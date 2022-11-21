5909e04ce79a105af3681c8a4e36012830c9a14c
Kamran Qadir <qadir0108@hotmail.com>
2022-11-21 10:02:06 +0500

    Initial commit

---
+++ HackerRankCSharpTest/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankCSharpTest
+{
+    internal class Program
+    {
+        public static Dictionary<string, int> AverageAgeForEachCompany(List<Employee> employees)
+        {
+            return employees
+                .GroupBy(x => x.Company)
+                .OrderBy(x => x.Key)
+                .Select(x => new { Key = x.Key, Avg = Convert.ToInt32(x.Average(y => y.Age)) })
+                .ToDictionary(x => x.Key, x => x.Avg);
+        }
+
+        public static Dictionary<string, int> CountOfEmployeesForEachCompany(List<Employee> employees)
+        {
+            return employees
+            .GroupBy(x => x.Company)
+            .OrderBy(x => x.Key)
+            .ToDictionary(x => x.Key, x => x.Count());
+        }
+
+        public static Dictionary<string, Employee> OldestAgeForEachCompany(List<Employee> employees)
+        {
+            return employees
+                .GroupBy(x => x.Company)
+                .OrderBy(x => x.Key)
+                .Select(x => new { Key = x.Key, Employee = x.FirstOrDefault(y => y.Age == Convert.ToInt32(x.Max(z => z.Age))) })
+                .ToDictionary(x => x.Key, x => x.Employee);
+        }
+
+        public static void Main()
+        {
+            int countOfEmployees = int.Parse(Console.ReadLine());
+
+            var employees = new List<Employee>();
+
+            for (int i = 0; i < countOfEmployees; i++)
+            {
+                string str = Console.ReadLine();
+                string[] strArr = str.Split(' ');
+                employees.Add(new Employee
+                {
+                    FirstName = strArr[0],
+                    LastName = strArr[1],
+                    Company = strArr[2],
+                    Age = int.Parse(strArr[3])
+                });
+            }
+
+            foreach (var emp in AverageAgeForEachCompany(employees))
+            {
+                Console.WriteLine($"The average age for company {emp.Key} is {emp.Value}");
+            }
+
+            foreach (var emp in CountOfEmployeesForEachCompany(employees))
+            {
+                Console.WriteLine($"The count of employees for company {emp.Key} is {emp.Value}");
+            }
+
+            foreach (var emp in OldestAgeForEachCompany(employees))
+            {
+                Console.WriteLine($"The oldest employee of company {emp.Key} is {emp.Value.FirstName} {emp.Value.LastName} having age {emp.Value.Age}");
+            }
+            Console.ReadKey();
+        }
+    }
+
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Company { get; set; }
+    }
+}
+
